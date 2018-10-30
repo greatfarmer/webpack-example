7617bb0885b753bbe0bd93b899df7bc7bd8c7fa4
minho.yoo <minho.yoo.dev@gmail.com>
2018-10-31 01:30:46 +0900

    first commit

---
+++ webpack-and-babel/src/index.js
@@ -0,0 +1,6 @@
+// index.js
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+// window.iAmJavascriptES6 = iAmJavascriptES6;
+iAmJavascriptES6();
+// https://medium.com/@benjaminwoojang/webpack-4%EC%99%80-babel-7%EC%9C%BC%EB%A1%9C-react%EA%B0%9C%EB%B0%9C%ED%99%98%EA%B2%BD-%EC%85%8B%EC%97%85%ED%95%98%EA%B8%B0-340e00d2760b
